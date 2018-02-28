d01bedda8c209eeb883bf264c56cf4ae9c55d691
Joshua Christensen <jcawesome@gmail.com>
2018-02-28 09:33:48 -0700

    update

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ var express = require('express');
 
 var app = express();
 
-var port = 3000;
+//process.env.PORT will pull the port out of the enviornment and if there is not one then it will use 3000
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+
+app.set('views','./src/views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send('hello world');
+    res.render('index', {list: ['a', 'b']});
 });
 
 app.get('/books', function(req, res){
     res.send('hello books');
 });
 
-app.listen(3000,function(err){
+app.listen(port,function(err){
     console.log('running server on port ' + port);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('style', function(){
-    gulp.src(jsFiles)
-        .pipe(jshint())
-        .pipe(jshint.reporter('rshint-stylish', {
-            verbose:true
-        }));
+gulp.task('serve', function() {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    }
+    
+    return nodemon(options)
+        .on('restart', function(){
+            console.log("restarting...");
+    });
 })
