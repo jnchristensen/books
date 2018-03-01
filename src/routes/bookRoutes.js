c3c14025fd5e30f31e80875ca0b50b1b6dc81978
Joshua Christensen <jcawesome@gmail.com>
2018-03-01 13:27:35 -0700

    css updates

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,39 +3,43 @@ var express = require('express');
 var bookRouter = express.Router();
 
 //we passed the nav in from app.js
-var router = function(nav){
+var router = function (nav) {
     var books = [{
         "Title": "Company",
         "Author": "Bettye Banton"
-      }, {
+    }, {
         "Title": "Duck Season (Temporada de patos)",
         "Author": "Ivor Sothcott"
-      }, {
+    }, {
         "Title": "There's No Business Like Show Business",
         "Author": "Mellisa Melbert"
-      }, {
+    }, {
         "Title": "On the Town",
         "Author": "Thacher Keymer"
-      }, {
+    }, {
         "Title": "Johnny Mnemonic",
         "Author": "Garald Hollingsby"
-      }];
-    
+    }];
+
     bookRouter.route('/')
-        .get(function(req,res){
-            res.render("books",{title: "hello from render",
-            nav: nav,
-            books: books});
-        });
-    
-        //The : will send up whatever is behind the slash and will store it in a variable in this case named id
-    bookRouter.route('/:id')
-        .get(function(req,res){
-                var id = req.params.id;
-                res.render("bookView",{title: "hello from render",
+        .get(function (req, res) {
+            res.render("books", {
+                title: "hello from render",
                 nav: nav,
-                book: books[id]
+                books: books
             });
+        });
+
+    //The : will send up whatever is behind the slash and will store it in a variable in this case named id
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = req.params.id;
+            res.render("bookView"
+                , {
+                    title: "hello from render",
+                    nav: nav,
+                    book: books[id]
+                });
         });
     return bookRouter;
 }
@@ -43,4 +47,4 @@ var router = function(nav){
 
 
 
-    module.exports = router;
+module.exports = router;
